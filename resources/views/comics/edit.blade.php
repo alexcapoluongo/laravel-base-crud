cb94738c6b9147e3b64386be9eb1d7c3e61efea9
alexcapoluongo <alexcapoluongo@icloud.com>
2022-07-04 15:28:10 +0200

    added function old in inputs

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -13,7 +13,7 @@
         </ul>
     </div>
     @endif
-    
+
     <form action="{{ route('comics.update', ['comic'=>$comic_to_update->id]) }}" method="POST">
         @csrf
         @method('PUT')
@@ -21,31 +21,32 @@
         <div class="mb-3">
             <label for="title" class="form-label">Titolo</label>
             <input type="text" class="form-control" id="title" name="title"
-            value="{{ $comic_to_update->title }}">
+            value="{{ old('title') ? old('title') : $comic_to_update->title }}">
         </div>
         <div class="mb-3">
             <label for="price" class="form-label">Prezzo</label>
             <input type="text" class="form-control" id="price" name="price"
-            value="{{ $comic_to_update->price }}">
+            value="{{ old('price') ? old('price') : $comic_to_update->price }}">
         </div>
         <div class="mb-3">
             <label for="image" class="form-label">Immagine</label>
             <input type="text" class="form-control" id="image" name="image"
-            value="{{ $comic_to_update->image }}">
+            value="{{ old('image') ? old('image') : $comic_to_update->image }}">
         </div>
         <div class="mb-3">
             <label for="type" class="form-label">Tipologia</label>
             <input type="text" class="form-control" id="type" name="type"
-            value="{{ $comic_to_update->type }}">
+            value="{{ old('type') ? old('type') : $comic_to_update->type }}">
         </div>
         <div class="mb-3">
             <label for="sale_date" class="form-label">Data di vendita</label>
             <input type="text" class="form-control" id="sale_date" name="sale_date"
-            value="{{ $comic_to_update->sale_date }}">
+            value="{{ old('sale_date') ? old('sale_date') : $comic_to_update->sale_date }}">
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Descrizione</label>
-            <textarea type="text" class="form-control" id="description" name="description">{{ $comic_to_update->description }}</textarea>
+            <input type="text" class="form-control" id="description" name="description"
+            value="{{ old('description') ? old('description') : $comic_to_update->description }}">
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
